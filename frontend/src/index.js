32bfc5ec437cb60dd050133b9db6b3f092388105
David Lee <davidlee1996@gmail.com>
2025-08-01 15:26:40 -0400

    index frontend work

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // optional, create an empty file if you like
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
